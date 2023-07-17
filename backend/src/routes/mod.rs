06b11a8abc55c1cec9d0312a2137d19c50414acd
Ujwal Jain <reachujwal@gmail.com>
2023-07-16 20:36:23 -0700

    Add CORS layer to router

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -6,6 +6,7 @@ use crate::app_state;
 use crate::routes::bdoc::{create_bdoc, get_bdoc, save_bdoc};
 use crate::routes::search::search_topic;
 use crate::routes::topic::create_topic;
+use axum::http::Method;
 use axum::middleware::map_response;
 use axum::{
     body::Body,
@@ -14,21 +15,29 @@ use axum::{
     routing::{get, post},
     Router,
 };
+use tower_http::cors::{Any, CorsLayer};
 
 pub fn create_routes(state: app_state::AppState) -> Router<(), Body> {
-    let unprotected_routes = Router::new()
+    let routes = Router::new()
         .route("/", get(|| async { "Hello, world!" }))
         .route("/topic/create", post(create_topic))
         .route("/search/topic", post(search_topic))
         .route("/bdoc/create", post(create_bdoc))
-        .route("/bdoc/get/:id", post(get_bdoc))
+        .route("/bdoc/get/:id", get(get_bdoc))
         .route("/bdoc/save/:id", post(save_bdoc));
 
     Router::new()
-        .merge(unprotected_routes)
+        .merge(routes)
         .layer(map_response(main_response_mapper))
+        .layer(cors())
         .with_state(state)
         .fallback(handler_404)
+}
+
+fn cors() -> CorsLayer {
+    CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any)
 }
 
 async fn main_response_mapper(res: Response) -> Response {

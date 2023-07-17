5337e79e53e8187f63a7896b3ec674dee76eccfc
Kartikeya Bhatt <krtky020@gmail.com>
2023-07-16 17:01:43 -0700

    Template

--- frontend/src/lib/index.ts
+++ frontend/src/lib/index.ts
@@ -1,1 +1,4 @@
-// place files you want to import through the `$lib` alias in this folder.
+export { default as MainPage } from './business_doc/MainPage.svelte'
+export { default as LeftRail } from './left_rail/LeftRail.svelte'
+export { default as TopBar } from './TopBar.svelte'
+export { default as RailCard } from './left_rail/RailCard.svelte'
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import {TopBar, LeftRail, MainPage} from '$lib'
+  
+  </script>
+  
+  <div class="app-container">
+    <TopBar />
+  
+    <div class="content-container">
+      <LeftRail />
+  
+      <div class="main-container">
+        <MainPage />
+      </div>
+    </div>
+  </div>
+  
+  <style>
+    .app-container {
+      display: flex;
+      flex-direction: column;
+      height: 100vh;
+    }
+  
+    .content-container {
+      display: flex;
+      flex: 1;
+      overflow: hidden;
+    }
+  
+    .main-container {
+      flex: 1;
+      overflow-y: auto;
+    }
+  </style>
+  
